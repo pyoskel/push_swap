610b6c01d47fa4151e4428bb83c45960954a5767
pyoskel <pasigermany@gmail.com>
2026-03-08 23:56:28 +0100

    updated the header: with the if not define, the library's and the struct s_node

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,25 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:33 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/01/24 19:15:36 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/03/07 22:01:28 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+/* --- Standard Bibliotheken --- */
+# include <stdlib.h> // for malloc and free
+# include <unistd.h> // for write
+
+/* --- Datenstrukturen --- */
+
+// Das ist unser "Listen-Element" (Node) für die Linked List
+typedef struct s_node
+{
+	int				value; // Die echte Zahl aus dem Input (z.B. -42, 100)
+	int				index; // Der Rang für deinen Algorithmus (z.B. 0, 1, 2...)
+	struct s_node	*next; // Der "Schnitzeljagd"-Zeiger auf das nächste Element
+}					t_node;
+
+#endif
