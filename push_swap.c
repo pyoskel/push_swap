9eec452406c033b4cbb63e1b3d1a6edc0b627c7f
pyoskel <pasigermany@gmail.com>
2026-03-08 20:55:14 +0100

    First start in the push_swap.c file, and implemented the argc and argv. With the case of (argc == 1) denn return (EXIT_FAILURE);. Started with the case of two arguments...

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,19 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:31 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/01/24 19:15:32 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/03/08 18:05:55 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (EXIT_FAILURE);
+	if (argc == 2)
+	{
+		
+	}
+	return (0);
+}
