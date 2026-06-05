20f760d1bfd465070813514b24217901eecc3c16
pabartoc <pabartoc@student.42heilbronn.de>
2026-06-05 03:18:17 +0200

    implement ft_split parsing for single string argument

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,19 +6,39 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:31 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/03/08 18:05:55 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/05 03:02:48 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "libft.h"
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	if (argc == 1)
+	char	**args;
+	// 1. Wenn keine Argumente oder ein leerer String übergeben wurden
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (EXIT_FAILURE);
 	if (argc == 2)
 	{
-		
+		args = ft_split(argv[1], ' ');
+		// FEHLER-ABFANGEN: Wenn ft_split fehlschlägt (Malloc-Fail)
+		if (!args)
+		{
+			ft_putstr_fd("Error\n", 2);
+			return (EXIT_FAILURE);
+		}
+			// === TEMP TEST-CODE ===
+		printf("--- TEST START ---\n");
+		int	i = 0;
+		while (args[i] != NULL)
+		{
+			printf("args[%d] = %s\n", i, args[i]);
+			i++;
+		}
+		printf("--- TEST ENDE ---\n");
+		// ============================
 	}
 	return (0);
 }
