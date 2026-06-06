95d85b1eb432c115743f045a33656929ad097294
pabartoc <pabartoc@student.42heilbronn.de>
2026-06-07 01:36:50 +0200

    I tidied things up and fixed an error

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:31 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/05 03:20:31 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/07 01:35:23 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,61 @@
 int	main(int argc, char **argv)
 {
 	char	**args;
-	// 1. Wenn keine Argumente oder ein leerer String übergeben wurden
+
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (EXIT_FAILURE);
 	if (argc == 2)
 	{
 		args = ft_split(argv[1], ' ');
-		// FEHLER-ABFANGEN: Wenn ft_split fehlschlägt (Malloc-Fail)
 		if (!args)
 		{
 			ft_putstr_fd("Error\n", 2);
 			return (EXIT_FAILURE);
 		}
-		else
-		{
-			// Wenn die Argumente einzeln übergeben wurden (z.B. ./push_swap 3 2 5)
-			// Wir überspringen argv[0] ("./push_swap") und zeigen direkt auf die Zahlen
-			args = argv + 1;
-		}
-			// === TEMP TEST-CODE ===
-		printf("--- TEST START ---\n");
-		int	i = 0;
-		while (args[i] != NULL)
-		{
-			printf("args[%d] = %s\n", i, args[i]);
-			i++;
-		}
-		printf("--- TEST ENDE ---\n");
-		// ============================
+	}
+	else
+	{
+		args = argv + 1;
 	}
 	return (0);
 }
+
+// === CODE ===
+// int	main(int argc, char **argv)
+// {
+// 	char	**args;
+// 	// 1. Wenn keine Argumente oder ein leerer String übergeben wurden
+// 	if (argc == 1 || (argc == 2 && !argv[1][0]))
+// 		return (EXIT_FAILURE);
+// 	if (argc == 2)
+// 	{
+// 		args = ft_split(argv[1], ' ');
+// 		// FEHLER-ABFANGEN: Wenn ft_split fehlschlägt (Malloc-Fail)
+// 		if (!args)
+// 		{
+// 			ft_putstr_fd("Error\n", 2);
+// 			return (EXIT_FAILURE);
+// 		}
+// 	}
+// 	else
+// 	{
+// 		// Wenn die Argumente einzeln übergeben wurden (z.B. ./push_swap 3 2 5)
+// 		// Wir überspringen argv[0] ("./push_swap")
+//		// und zeigen direkt auf die Zahlen
+// 		args = argv + 1;
+// 	}
+	// 	// === TEMP TEST-CODE ===
+	// printf("--- TEST START ---\n");
+	// int	i = 0;
+	// while (args[i] != NULL)
+	// {
+	// 	printf("args[%d] = %s\n", i, args[i]);
+	// 	i++;
+	// }
+// 	printf("--- TEST ENDE ---\n");
+// 	// ============================
+// 	// make
+// 	// ./push_swap "42 1337 -5 0"
+// 	// ./push_swap 42 1337 -5 0
+// 	// ============================
+// 	return (0);
