f19456ad0cb076dea8c321455fd2847d4da0309f
pyoskel <pasigermany@gmail.com>
2026-06-21 19:46:05 +0200

    I corrected a typographical error (typo). I mixed up args and argc.

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:31 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/21 19:37:35 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/21 19:43:49 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	main(int argc, char **argv)
 		args = argv + 1;
 	if (!is_input_valid(args))
 	{
-		if (args == 2)
+		if (argc == 2)
 			free_args(args);
 		return (ft_putstr_fd("Error\n", 2), EXIT_FAILURE);
 	}
@@ -57,7 +57,7 @@ int	main(int argc, char **argv)
 		if (!new_node)
 		{
 			free_stack(&stack_a);
-			if (args == 2)
+			if (argc == 2)
 				free_args(args);
 			return (ft_putstr_fd("Error\n", 2), EXIT_FAILURE);
 		}
