442aa6654e49ed1f5ff32b758b7e366c44f3389b
pyoskel <pasigermany@gmail.com>
2026-06-24 01:16:22 +0200

    includet radix sorting algo

--- src/radix.c
+++ src/radix.c
@@ -6,11 +6,44 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 19:04:48 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/23 21:40:31 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/24 00:36:54 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../inc/push_swap.h"
+
+static int	get_stack_size(t_node *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
+}
+
+static int	get_max_bits(t_node **stack)
+{
+	t_node	*head;
+	int		max;
+	int		max_bits;
+
+	head = *stack;
+	max = head->index;
+	max_bits = 0;
+	while (head)
+	{
+		if (head->index > max)
+			max = head->index;
+		head = head->next;
+	}
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
+}
 
 void	radix_sort(t_node **stack_a, t_node **stack_b)
 {
@@ -19,5 +52,22 @@ void	radix_sort(t_node **stack_a, t_node **stack_b)
 	int	i;
 	int	j;
 
-	
+	size = get_stack_size(*stack_a);
+	max_bits = get_max_bits(stack_a);
+	i = 0;
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if ((((*stack_a)->index) >> i) & 1)
+				ra(stack_a);
+			else
+				pb(stack_a, stack_b);
+			j++;
+		}
+		while (*stack_b)
+			pa(stack_a, stack_b);
+		i++;
+	}
 }
