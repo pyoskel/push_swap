fec9a84573da613c9bd4a311e1b8e31c47c20fda
pyoskel <pasigermany@gmail.com>
2026-06-25 02:31:38 +0200

    I've fixed the norminette case for 25 Lines for a function (the main() function).

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,13 +6,13 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 19:15:31 by pabartoc          #+#    #+#             */
-/*   Updated: 2026/06/25 00:26:56 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/06/25 02:22:27 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 #include "libft.h"
-#include <stdio.h> // temporarily
+// #include <stdio.h> // temporarily
 
 // Temporäre Hilfsfunktion, um den fertigen Stack zu sehen
 // void	print_stack(t_node *stack, char stack_name)
@@ -25,6 +25,33 @@
 // 	}
 // 	printf("NULL\n");
 // }
+
+static int	init_stack(t_node **stack_a, char **args)
+{
+	t_node	*new_node;
+	int		i;
+
+	i = 0;
+	while (args[i])
+	{
+		new_node = stack_new_node(ft_atoi(args[i]));
+		if (!new_node)
+			return (0);
+		stack_add_back(stack_a, new_node);
+		i++;
+	}
+	return (1);
+}
+
+static int	free_and_error(t_node **stack_a, char **args, int argc)
+{
+	if (argc == 2 && args)
+		free_args(args);
+	if (stack_a && *stack_a)
+		free_stack(stack_a);
+	ft_putstr_fd("Error\n", 2);
+	return (1);
+}
 
 static int	is_sorted(t_node *stack)
 {
@@ -39,59 +66,43 @@ static int	is_sorted(t_node *stack)
 	return (1);
 }
 
+static void	sort_stacks(t_node **stack_a, t_node **stack_b)
+{
+	int	size;
+
+	size = get_stack_size(*stack_a);
+	if (size == 2)
+		sa(stack_a);
+	else if (size == 3)
+		sort_3(stack_a);
+	else if (size <= 5)
+		sort_4_5(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
 int	main(int argc, char **argv)
 {
 	char	**args;
-	t_node	*new_node;
 	t_node	*stack_a;
 	t_node	*stack_b;
-	int		i;
 
 	stack_a = NULL;
 	stack_b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (EXIT_FAILURE);
 	if (argc == 2)
-	{
 		args = ft_split(argv[1], ' ');
-		if (!args)
-			return (ft_putstr_fd("Error\n", 2), EXIT_FAILURE);
-	}
 	else
 		args = argv + 1;
-	if (!is_input_valid(args))
-	{
-		if (argc == 2)
-			free_args(args);
-		return (ft_putstr_fd("Error\n", 2), EXIT_FAILURE);
-	}
-	i = 0;
-	while (args[i])
-	{
-		new_node = stack_new_node(ft_atoi(args[i]));
-		if (!new_node)
-		{
-			free_stack(&stack_a);
-			if (argc == 2)
-				free_args(args);
-			return (ft_putstr_fd("Error\n", 2), EXIT_FAILURE);
-		}
-		stack_add_back(&stack_a, new_node);
-		i++;
-	}
+	if (!is_input_valid(args) || !init_stack(&stack_a, args))
+		return (free_and_error(&stack_a, args, argc));
 	if (argc == 2)
 		free_args(args);
 	if (!is_sorted(stack_a))
 	{
 		index_stack(&stack_a);
-		if (get_stack_size(stack_a) == 2)
-			sa(&stack_a);
-		else if (get_stack_size(stack_a) == 3)
-			sort_3(&stack_a);
-		else if (get_stack_size(stack_a) <= 5)
-			sort_4_5(&stack_a, &stack_b);
-		else
-			radix_sort(&stack_a, &stack_b);
+		sort_stacks(&stack_a, &stack_b);
 	}
 	free_stack(&stack_a);
 	free_stack(&stack_b);
